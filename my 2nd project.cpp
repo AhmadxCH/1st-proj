0f8eaddf752a30e43548fea913b7b09f76a00f63
AhmadxCH <imahmad1414@gmail.com>
2024-10-21 17:55:00 +0500

    Add files via upload

---
+++ my 2nd project.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+using namespace std;
+int main()
+task 1
+//{/*
+//	cout << "*************
+//	cout << "Hello world" << endl;
+//	cout << "*************"<<endl; 
+return 0;
+}
+task 2 
+{
+//cout << "*************" << endl;
+//cout << "*           *" << endl;
+//cout << "*************" << endl;
+//return 0;*/
+//}
+  //task 3 
+  //{
+	 // cout << "        *" << endl;
+	 // cout << "       * *" << endl;
+	 // cout << "      *   *" << endl;
+	 // cout << "     *******" << endl;
+	 // return 0;
+ // //}
+	//task 4
+/*
+    cout << "        *" << endl;
+    cout << "       ***" << endl;
+    cout << "      *****" << endl;
+    cout << "     *******" << endl;
+    return 0;
+}*/
+//task 5
+//{
+//    cout << "       ****" << endl;
+//    cout << "      *   *" << endl;
+//    cout << "      *   *" << endl;
+//    cout << "      ****" << endl;
+//    return 0;
+//}
+//task 6
+//{ 
+//    cout << "  ******** " << endl;
+//    cout << "  *      *" << endl;
+//    cout << "  *      *" << endl;
+//    cout << "  ********" << endl;
+//    return 0;
+//}
+//task 7
+//{
+//	   cout << "    *" << endl;
+//	   cout << "  *   *" << endl;
+//	   cout << " *     *" << endl;
+//     cout << "  *   * " << endl;
+//	   cout << "    *   " << endl;
+//	   return 0;
+//	}
+//task 8 
+/* { */ /*
+   cout << "        *" << endl;
+   cout << "      ***** " << endl;
+   cout << "     *******" << endl;
+   cout << "      *****" << endl;
+   cout << "        *  " << endl;
+   return 0;
+}*/
+// task 9 
+// {
+// 	    cout << "@@@@@@@@@@@@@@" << endl;
+//      cout << "@  @      @  @" << endl;
+//      cout << "@ @        @ @" << endl;
+//      cout << "@@          @@" << endl;
+//      cout << "@            @" << endl;
+//      cout << "@@@@@@@@@@@@@@" << endl;
+//    return 0;
+// }
+// task 10 
+// {
+// 	   cout << "****************" << endl;
+//        cout << "        *       " << endl;
+//        cout << "        *       " << endl;
+//        cout << "        *       " << endl;
+//        cout << "        *       " << endl;
+//        cout << "        *       " << endl;
+//        cout << "        *       " << endl;
+//        cout << "****************" << endl;
+//        cout << "****************" << endl;
+//        cout << "        *       " << endl;
+//        cout << "        *       " << endl;
+//        cout << "        *       " << endl;
+//        cout << "        *       " << endl;
+//        cout << "        *       " << endl;
+//        cout << "        *       " << endl;
+//        cout << "        *       " << endl;
+//        cout << "        *       " << endl;
+//        cout << "****************" << endl;
+//        cout << "*               " << endl;
+//        cout << "*               " << endl;
+//        cout << "*               " << endl;
+//        cout << "*               " << endl;
+//        cout << "*               " << endl;
+//        cout << "*               " << endl;
+//        cout << "*               " << endl;
+//        cout << "*               " << endl;
+//        cout << "****************" << endl;
+//        reurn 0;
+// } 
+// task 11 
+// {
+//     cout << "Name:Ali Ahmad" << endl;
+//     cout << "University id:L1F24BSDS0012" << endl;
+//     cout << "Father Name:Muhammad Ahmad" << endl;
+//     cout << "Phone Number:0324123456" << endl;
+//     cout << "Adress:Block AA,House no1,Street no2,Johar Town,Lahore" << endl;
+//     cout << "Semester:1"<< endl;
+//     cout << "Course:Introduction to computing lab"<< endl;
+//     return 0;
+// }
+// task 12 
+// { 
+//     cout << "**********************************" << endl;
+//     cout << "*    Programming assignment 1    *" << endl;
+//     cout << "*    Computer programming 1      *" << endl;
+//     cout << "*        Auhor: ???              *" << endl;
+//     cout << "*    Due Date: Thursday, Jan.24  *" << endl;
+//     cout << "**********************************" << endl;
+//     return 0;
+// }
